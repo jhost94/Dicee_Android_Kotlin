9db610347cf5ea681b280736b2fa23495028cac7
Fábio Cardoso <fabio.cardoso@bringglobal.com>
2021-09-10 11:06:14 +0000

    Basic kotlin conversion

---
+++ app/src/main/java/com/jhost/dicee/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.jhost.dicee
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.random.Random
+
+class MainActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        log("Hello world!")
+
+        init()
+    }
+
+    private fun init() {
+        log("I WAS HERE")
+        val dices = intArrayOf(
+            R.drawable.dice1,
+            R.drawable.dice2,
+            R.drawable.dice3,
+            R.drawable.dice4,
+            R.drawable.dice5,
+            R.drawable.dice6
+        )
+
+        val rollButton: Button = findViewById(R.id.rollbutton)
+        val leftDice: ImageView = findViewById(R.id.leftDice)
+        val rightDice: ImageView = findViewById(R.id.rightDice)
+
+        setRandomDice(leftDice, dices)
+        setRandomDice(rightDice, dices)
+    }
+
+    private fun setRandomDice(imageView: ImageView, dices: IntArray){
+        log(Random(dices.size).nextInt().toString())
+        imageView.setImageResource(dices[Random(dices.size).nextInt()])
+    }
+
+    private fun log(msg: String) {
+        Log.d("Dicee", msg)
+    }
+}
